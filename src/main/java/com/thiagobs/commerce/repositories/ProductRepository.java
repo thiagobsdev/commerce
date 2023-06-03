984518c8474ad1b78588df9f80c8d6d93d669bbb
Thiago Bs <thiago-fa@hotmail.com>
2023-06-03 16:58:26 -0300

    Query personalizada

--- src/main/java/com/thiagobs/commerce/repositories/ProductRepository.java
+++ src/main/java/com/thiagobs/commerce/repositories/ProductRepository.java
@@ -1,11 +1,20 @@
 package com.thiagobs.commerce.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.thiagobs.commerce.entities.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	
+	@Query("SELECT obj "
+			+ "FROM Product obj "
+			+ "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+	Page<Product> searchByName( String name, Pageable page);
 
 }
